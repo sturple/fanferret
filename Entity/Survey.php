6ef0c4ff65f04cba3ab8207ca3024767158ff292
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-05-04 22:36:24 -0700

    Entity\Survey & Entity\SurveyTranslation

---
+++ Entity/Survey.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace QuestionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="QuestionBundle\Repository\SurveyRepository")
+ * @ORM\Table(name="survey")
+ */
+class Survey
+{
+    
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $companyId;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="SurveyTranslation",mappedBy="survey")
+     */
+    private $translations;
+    
+    /**
+     * @ORM\Column(type="string",length=255)
+     */
+    private $slug;
+    
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->translations = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set companyId
+     *
+     * @param integer $companyId
+     *
+     * @return Survey
+     */
+    public function setCompanyId($companyId)
+    {
+        $this->companyId = $companyId;
+
+        return $this;
+    }
+
+    /**
+     * Get companyId
+     *
+     * @return integer
+     */
+    public function getCompanyId()
+    {
+        return $this->companyId;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Survey
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Add translation
+     *
+     * @param \QuestionBundle\Entity\SurveyTranslation $translation
+     *
+     * @return Survey
+     */
+    public function addTranslation(\QuestionBundle\Entity\SurveyTranslation $translation)
+    {
+        $this->translations[] = $translation;
+
+        return $this;
+    }
+
+    /**
+     * Remove translation
+     *
+     * @param \QuestionBundle\Entity\SurveyTranslation $translation
+     */
+    public function removeTranslation(\QuestionBundle\Entity\SurveyTranslation $translation)
+    {
+        $this->translations->removeElement($translation);
+    }
+
+    /**
+     * Get translations
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getTranslations()
+    {
+        return $this->translations;
+    }
+}
---
+++ Entity/SurveyTranslation.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace QuestionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="QuestionBundle\Repository\SurveyTranslationRepository")
+ * @ORM\Table(name="survey_translation")
+ */
+class SurveyTranslation
+{
+    
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Survey",inversedBy="translations")
+     */
+    private $survey;
+    
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+    
+    
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set survey
+     *
+     * @param \QuestionBundle\Entity\Survey $survey
+     *
+     * @return SurveyTranslation
+     */
+    public function setSurvey(\QuestionBundle\Entity\Survey $survey = null)
+    {
+        $this->survey = $survey;
+
+        return $this;
+    }
+
+    /**
+     * Get survey
+     *
+     * @return \QuestionBundle\Entity\Survey
+     */
+    public function getSurvey()
+    {
+        return $this->survey;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return SurveyTranslation
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+}
