bedac5f4aca8034a114b337cdeeeb16a19eebeab
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-05-04 19:24:11 -0700

    Translation Entity

---
+++ Entity/Translation.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace QuestionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Translation
+ *
+ * @ORM\Table(name="translation",uniqueConstraints={@ORM\UniqueConstraint(name="translation_language_idx",columns={"id","language"})})
+ * @ORM\Entity(repositoryClass="QuestionBundle\Repository\TranslationRepository")
+ */
+class Translation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="language", type="string", length=255)
+     */
+    private $language;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="string", type="text")
+     */
+    private $string;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set language
+     *
+     * @param string $language
+     *
+     * @return Translation
+     */
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    /**
+     * Get language
+     *
+     * @return string
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * Set string
+     *
+     * @param string $string
+     *
+     * @return Translation
+     */
+    public function setString($string)
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    /**
+     * Get string
+     *
+     * @return string
+     */
+    public function getString()
+    {
+        return $this->string;
+    }
+}
+
