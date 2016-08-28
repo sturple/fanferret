5c994dbc3d07e691c6d9ca484f6e8e964f58b083
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-08-28 14:36:51 -0700

    \FanFerret\QuestionBundle\Question\QuestionInterface::getAnswer Documentation Clarification

--- Question/QuestionInterface.php
+++ Question/QuestionInterface.php
@@ -28,6 +28,13 @@ interface QuestionInterface
      * Gets a QuestionAnswer entity representing the
      * answer to this question.
      *
+     * This method shall link the QuestionAnswer to
+     * the Question entity this object represents,
+     * but shall not form the relationship the other
+     * way.  This is to ensure that should the caller
+     * decide not to use the returned QuestionAnswer
+     * entity the wrapped Question remains unaffected.
+     *
      * @param array $data
      *  An associative array of data from the submitted
      *  form.
