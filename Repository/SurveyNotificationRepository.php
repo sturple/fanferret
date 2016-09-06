561f8bbece6c6ee0e02c701a5f6fc3dbc25a7d94
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-09-06 01:29:13 -0700

    \FanFerret\QuestionBundle\Repository\SurveyNotificationRepository::getByToken

--- Repository/SurveyNotificationRepository.php
+++ Repository/SurveyNotificationRepository.php
@@ -10,4 +10,24 @@ namespace FanFerret\QuestionBundle\Repository;
  */
 class SurveyNotificationRepository extends \Doctrine\ORM\EntityRepository
 {
+    /**
+     * Attempts to retrieve the SurveyNotification
+     * which has a particular token.
+     *
+     * @param string $token
+     *
+     * @return SurveyNotification|null
+     */
+    public function getByToken($token)
+    {
+        $qb = $this->createQueryBuilder('sn');
+        $where = $qb->expr()->eq('sn.token',':token');
+        $qb->andWhere($where)
+            ->setParameter('token',$token)
+            ->setMaxResults(1);
+        $q = $qb->getQuery();
+        $arr = $q->getResult();
+        if (count($arr) !== 1) return null;
+        return $arr[0];
+    }
 }
