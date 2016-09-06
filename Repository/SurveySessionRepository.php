2015c10e3a7cbcf1c648fb574123192f45f7b382
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-09-05 22:25:19 -0700

    \FanFerret\QuestionBundle\Repository\SurveySessionRepository::getByNotificationCount => getByNotification
    Added the $since parameter.

--- Repository/SurveySessionRepository.php
+++ Repository/SurveySessionRepository.php
@@ -32,20 +32,47 @@ class SurveySessionRepository extends \Doctrine\ORM\EntityRepository
 
     /**
      * Attempts to retrieve all SurveySession entities
-     * which have a certain number of notifications.
+     * which have a certain number of notifications and
+     * whose last notification was sent a certain amount
+     * of time ago.
      *
      * @param int $count
+     * @param DateInterval|null $since
      *
      * @return array
      */
-    public function getByNotificationCount($count)
+    public function getByNotification($count, \DateInterval $since = null)
     {
+        //  Sanity check arguments
+        if (($count === 0) && !is_null($since)) throw new \InvalidArgumentException(
+            'If $count is 0 $since must be NULL'
+        );
+        if ($count < 0) throw new \InvalidArgumentException(
+            'Negative count'
+        );
+        //  Build query
         $qb = $this->createQueryBuilder('ss');
         $count_expr = $qb->expr()->count('sn.id');
-        $having_expr = $qb->expr()->eq($count_expr,$count);
+        $having_count_expr = $qb->expr()->eq($count_expr,$count);
         $qb->leftJoin('ss.surveyNotifications','sn')
             ->addGroupBy('ss.id')
-            ->andHaving($having_expr);
+            ->andHaving($having_count_expr);
+        //  Handle date/time constraint
+        if (!is_null($since)) {
+            //  Sanity check
+            $now = new \DateTime();
+            $when = clone $now;
+            $when->sub($since);
+            if ($when->getTimestamp() > $now->getTimestamp()) throw new \InvalidArgumentException(
+                '$since is negative interval'
+            );
+            //  Add to query
+            $max_expr = $qb->expr()->max('sn.when');
+            $having_max_expr = $qb->expr()->gte($max_expr,':when');
+            $qb->andHaving($having_max_expr)
+                ->setParameter('when',$when);
+        }
+        //  Execute query
         $q = $qb->getQuery();
         return $q->getResult();
     }
