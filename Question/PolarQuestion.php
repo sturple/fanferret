4623aa736c65f6b475fd0b16d031993ed3fdec8b
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-09-08 23:46:30 -0700

    \FanFerret\QuestionBundle\Question\PolarQuestion - Explain Support
    Either the positive or negative answer to a polar question may now be tied to a "please explain" field.

--- Question/PolarQuestion.php
+++ Question/PolarQuestion.php
@@ -9,18 +9,37 @@ class PolarQuestion extends Question
 {
 	private $negative;
 	private $twig;
+	private $explain;
 
 	public function __construct(\FanFerret\QuestionBundle\Entity\Question $q, \FanFerret\QuestionBundle\Internationalization\TranslatorInterface $t, \Twig_Environment $twig)
 	{
 		parent::__construct($q,$t);
 		$this->negative = $this->getBoolean('negative');
 		$this->twig = $twig;
+		$this->explain = $this->getOptionalString('explain');
+		if (!is_null($this->explain)) switch ($this->explain) {
+			case 'positive':
+			case 'negative':
+				break;
+			default:
+				throw new \InvalidArgumentException(
+					sprintf(
+						'Expected "explain" to be "positive" or "negative" got "%s"',
+						$this->explain
+					)
+				);
+		}
 	}
 
 	public function addToFormBuilder(\Symfony\Component\Form\FormBuilderInterface $fb)
 	{
+		$name = $this->getName();
 		$fb->add(
-			$this->getName(),
+			$name,
+			\Symfony\Component\Form\Extension\Core\Type\HiddenType::class
+		);
+		if ($this->explain) $fb->add(
+			$name . '_explain',
 			\Symfony\Component\Form\Extension\Core\Type\HiddenType::class
 		);
 	}
@@ -28,7 +47,22 @@ class PolarQuestion extends Question
 	public function getAnswer(array $data)
 	{
 		$retr = parent::getAnswer($data);
-		$retr->setValue($data[$this->getName()] ? 'true' : 'false');
+		$name = $this->getName();
+		$val = !!$data[$name];
+		if (!$this->explain) {
+			$retr->setValue($val ? 'true' : 'false');
+			return $retr;
+		}
+		$ans = (object)[
+			'value' => $val,
+			'explain' => null
+		];
+		$explain_positive = $this->explain === 'positive';
+		$is_positive = $val !== $this->negative;
+		if ($explain_positive === $is_positive) {
+			$ans->explain = $data[$name . '_explain'];
+		}
+		$retr->setValue(json_encode($ans));
 		return $retr;
 	}
 
@@ -37,7 +71,8 @@ class PolarQuestion extends Question
 		return $this->twig->render(
 			'FanFerretQuestionBundle:Question:polar.html.twig',
 			$this->getRenderContext([
-				'negative' => $this->negative
+				'negative' => $this->negative,
+				'explain' => $this->explain
 			])
 		);
 	}
--- Resources/public/js/survey/question/polar.js
+++ Resources/public/js/survey/question/polar.js
@@ -2,11 +2,43 @@ define(['jquery','survey/question/base'],function ($, base) {
 	return function (name, group, document) {
 		base.call(this,name,group,document);
 		document = $(document);
-		var check = document.find('#' + name + ' input[type="checkbox"]');
+		var root = document.find('#' + name);
+		var check = root.find('input[type="checkbox"]');
 		var hidden = document.find('#form_' + name);
+		var negative = root.hasClass('fanferret-polar-negative');
+		var explain_div = null;
+		var explain = null;
+		var explain_hidden = null;
+		var explain_negative = root.hasClass('fanferret-polar-explain-negative');
+		var is_explain = function () {
+			var is_negative = check[0].checked === negative;
+			return is_negative === explain_negative;
+		};
+		if (root.hasClass('fanferret-polar-explain')) {
+			explain_div = root.find('.fanferret-polar-explain-input');
+			explain = root.find('input[type="text"]');
+			explain_hidden = document.find('#form_' + name + '_explain');
+			var update = function () {
+				explain_hidden.val(explain.val());
+				group.update();
+			};
+			update();
+			explain.change(update);
+			var valid = this.valid;
+			this.valid = function () {
+				if (!is_explain()) return valid();
+				if (explain.val().trim() === '') return false;
+				return valid();
+			};
+		}
 		var set = function (value) {
 			hidden.val(value ? 'true' : '');
 			check.prop('checked',value);
+			if (explain) {
+				if (is_explain()) explain_div.show();
+				else explain_div.hide();
+			}
+			group.update();
 		};
 		set(check.prop('checked'));
 		check.change(function () {
