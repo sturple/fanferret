63d9d3476a4c3e22d2f628e258862fdcbb04ce9f
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-09-21 03:13:24 -0700

    localStorage - Open Type Questions

--- Resources/public/js/survey/question/open.js
+++ Resources/public/js/survey/question/open.js
@@ -7,10 +7,16 @@ define(['jquery','survey/question/base'],function ($, base) {
 		var response = div.find('textarea');
 		var testimonial = null;
 		var testimonial_hidden = null;
+		var key = group.getToken() + '_' + name;
+		var testimonial_key = key + '_testimonial';
 		var update = function () {
-			hidden.val(response.val());
+			var val = response.val();
+			hidden.val(val);
+			storage.setItem(key,val);
 			if (!testimonial) return;
-			testimonial_hidden.val(testimonial.prop('checked') ? 'true' : '');
+			var tval = testimonial.prop('checked') ? 'true' : '';
+			testimonial_hidden.val(tval);
+			storage.setItem(testimonial_key,tval);
 		};
 		response.on('input change',update);
 		if (div.hasClass('fanferret-testimonial')) {
@@ -18,6 +24,8 @@ define(['jquery','survey/question/base'],function ($, base) {
 			testimonial.change(update);
 			testimonial_hidden = document.find('#form_' + name + '_testimonial');
 		}
+		response.val(storage.getItem(key));
+		if (testimonial) testimonial.prop('checked',storage.getItem(testimonial_key) === 'true');
 		update();
 	};
 });
