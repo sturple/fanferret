273942302039a62bee0f18d5067476c2f3133e17
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-09-21 03:31:24 -0700

    localStorage - Rating Type Questions

--- Resources/public/js/survey/question/rating.js
+++ Resources/public/js/survey/question/rating.js
@@ -20,16 +20,19 @@ define(['jquery','survey/question/base','survey/condition','survey/radio'],funct
 			if (v === null) return false;
 			return c.check(v);
 		};
-		var get_explain = function () {
-			if (!is_explain()) return null;
-			return explain.val();
-		};
+		var key = group.getToken() + '_' + name;
+		var explain_key = key + '_explain';
 		var update = function () {
-			hidden.val(get_value());
+			var val = get_value();
+			hidden.val(val);
+			if (val === null) storage.removeItem(key);
+			else storage.setItem(key,val);
 			if (explain_hidden) {
 				if (is_explain()) explain_div.slideDown();
 				else explain_div.slideUp();
-				explain_hidden.val(get_explain());
+				var ex = explain.val();
+				explain_hidden.val(ex);
+				storage.setItem(explain_key,ex);
 			}
 			group.update();
 		};
@@ -39,6 +42,16 @@ define(['jquery','survey/question/base','survey/condition','survey/radio'],funct
 			explain_div = div.find('.fanferret-rating-explain-input');
 			c = new condition(threshold,operator);
 		}
+		this.addValid(function () {
+			return get_value() !== null;
+		});
+		var restored = storage.getItem(key);
+		if (restored !== null) {
+			radios.setValue(restored);
+		}
+		if (explain_hidden) {
+			explain.val(storage.getItem(explain_key));
+		}
 		update();
 		var prev = radios.change;
 		radios.change = function () {
@@ -46,8 +59,5 @@ define(['jquery','survey/question/base','survey/condition','survey/radio'],funct
 			update();
 		};
 		if (explain) explain.on('input change',update);
-		this.addValid(function () {
-			return get_value() !== null;
-		});
 	};
 });
--- Resources/public/js/survey/radio.js
+++ Resources/public/js/survey/radio.js
@@ -3,15 +3,25 @@ define(['jquery'],function ($) {
 		var curr = null;
 		var self = this;
 		es = $(es);
-		es.click(function () {
-			var e = $(this);
+		var select = function (e) {
 			curr = e.attr('data-value');
 			es.removeClass('selected');
 			e.addClass('selected');
 			self.change(curr,this);
+		};
+		es.click(function () {
+			select($(this));
 		});
 		this.getValue = function () {
 			return curr;
+		};
+		this.setValue = function (val) {
+			for (var i = 0; i < es.length; ++i) {
+				var e = $(es[i]);
+				if (e.attr('data-value') !== val) continue;
+				select(e);
+				break;
+			}
 		};
 		this.change = function () {	};
 	};
