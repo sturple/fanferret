d2be821be88f62031853564d27f9893ed3d3154b
shawn <shawn@turple.ca>
2016-11-02 13:00:31 -0400

    Updated styles for fanferret question bundle to add less pre-processor by using grunt. Added less files for admin and survey. All style sheets are automatically created.

---
+++ Resources/public/src/Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      scripts: {
+        files: ['less/**/*.less'],
+        tasks: ['less'],
+        options: {
+          spawn: false,
+        },
+      },
+    },    
+    less: {
+      development: {
+        options: {
+            paths: ['less/'],
+          compress: false,
+          yuicompress: true,
+          syncImport: true,
+          strictImports: true          
+        },
+        files: {
+          "../css/admin.css": ["less/mixins.less","less/admin.less"] ,
+          "../css/survey.css" : ["less/mixins.less","less/survey.less"],
+          "../css/thepalmstc.css" : ["less/mixins.less","less/thepalmstc.less"]
+        }
+      }
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task(s).
+  grunt.registerTask('default', ['less']);
+
+};
