268e9ccb07e117b0895922c362e5c1bbe3a72b67
sturple <shawn.turple@shaw.ca>
2017-01-31 14:34:21 -0800

    Updated CheckboxQuestion.php to allow an integer or string(multi-select)

--- Question/CheckboxQuestion.php
+++ Question/CheckboxQuestion.php
@@ -31,25 +31,38 @@ class CheckboxQuestion extends Question
 
     private function getOption($option)
     {
-        if (!is_numeric($option)) throw new \InvalidArgumentException(
-            sprintf(
-                'Option "%s" is not numeric',
-                $option
-            )
-        );
-        $i = intval($option);
-        if ($i != floatval($option)) throw new \InvalidArgumentException(
-            sprintf(
-                'Option "%s" is not integer',
-                $option
-            )
-        );
-        if (($i<0) || ($i>=count($this->options))) throw new \InvalidArgumentException(
-            sprintf(
-                'Option index %d out of range',
-                $i
-            )
-        );
+        if (!is_numeric($option)) {
+            $array = explode(',',$option);
+            if (is_array($array) and count($array) > 0 ){
+                $i = $array;
+            }
+            else {
+                throw new \InvalidArgumentException(
+                    sprintf(
+                        'Option "%s" is not integer or cannot be converted to array',
+                        $option
+                    )
+                );
+            }
+                
+            
+        }
+        else {
+            $i = intval($option);
+            if ($i != floatval($option)) throw new \InvalidArgumentException(
+                sprintf(
+                    'Option "%s" is not integer',
+                    $option
+                )
+            );
+            if (($i<0) || ($i>=count($this->options))) throw new \InvalidArgumentException(
+                sprintf(
+                    'Option index %d out of range',
+                    $i
+                )
+            );            
+        }
+
         return $i;
     }
 
@@ -62,8 +75,7 @@ class CheckboxQuestion extends Question
             $key = $name . '_other';
             $other = $data[$key];
             if ($other) {
-                $retr->other = $other;
-                return $retr;
+                $retr->other = $other;               
             }
         }
         $option = $data[$name];
